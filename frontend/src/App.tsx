ea0d4221bbd67c6b2c44c1a8e46c2e9ed18647f5
sophielluo <ssophieluo@gmail.com>
2025-06-21 15:00:31 -0400

    frontend skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import UserInterface from "./pages/UserInterface";
+import FocusGroup from "./pages/FocusGroup";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -15,7 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<UserInterface />} />
+          <Route path="/personas" element={<Index />} />
+          <Route path="/focus-group" element={<FocusGroup />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
